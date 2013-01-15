7e6d8476fe1d221fe10118baad23bd3b1ca10a55
meirish <matthew.irish@gmail.com>
2013-01-14 23:17:19 -0600

    Deployed on Tue Jan 15 05:17:19 UTC 2013

--- site_deploy.sh
+++ site_deploy.sh
@@ -1,17 +1,28 @@
 #!/bin/bash
 
+
+echo "Compiling static assets"
+compass compile -e production --force
+
 echo "Generating site"
 jekyll
+
 echo "Emptying deploy folder"
 rm -rf deploy/*
+
 echo "Copying site files to deploy folder"
 cp -r _site/* deploy
+
 echo "Commiting static files to master"
 cd deploy/
 git add .
 git add -u
 currtime=`date -u`
-git commit -m "Deploy at ${currtime}"
+git commit -m "Deployed on ${currtime}"
+
 echo "Pushing latest"
 git push origin master --force
+
+# put us back where we started
+cd ..
 echo "All done! Check it out at http://designiseasy.github.com"
