2fac4616c7c80e38c408dcb9cc64481421351641
meirish <matthew.irish@gmail.com>
2013-01-14 22:39:49 -0600

    Deploy at Tue Jan 15 04:39:49 UTC 2013

---
+++ deploy.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+echo "Generating site"
+jekyll
+echo "Emptying deploy folder"
+rm -rf deploy/*
+echo "Copying site files to deploy folder"
+cp -r _site/* deploy
+echo "Commiting static files to master"
+cd deploy/
+git add .
+git add -u
+currtime=`date -u`
+git commit -m "Deploy at ${currtime}"
+echo "Pushing latest"
+git push origin master --force
+echo "All done! Check it out at http://designiseasy.github.com"
