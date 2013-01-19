df2cd09776de3c5f463e5cae0afb2865402266e0
meirish <matthew.irish@gmail.com>
2013-01-18 22:11:10 -0600

    Deployed on Sat Jan 19 04:11:10 UTC 2013

--- static/js/application.js
+++ static/js/application.js
@@ -1,1 +1,57 @@
-// Your JavaScript
+;(function (win){
+  var doc = win.document,
+    main,
+    getPage,
+    replacePage,
+    pushUrl;
+
+  getPage = function (url, callback) {
+    req = new XMLHttpRequest();
+
+    req.addEventListener('readystatechange', function () {
+       if (req.readyState === 4) {
+        if (req.status === 200 || req.status === 304){
+          callback(req.responseText);
+        }
+       }
+    }, false);
+
+    req.open('GET', url, false);
+    req.send();
+  };
+
+  replacePage = function (content) {
+    var mainEle = doc.getElementById('main'),
+      htmlDoc = doc.implementation.createHTMLDocument(doc.title);
+    htmlDoc.body.innerHTML = content;
+    mainEle.innerHTML = htmlDoc.getElementById('main').innerHTML;
+  };
+
+  pushUrl = function (url) {
+    win.history.pushState({}, doc.title, url);
+  };
+
+  delegate = new Delegate(doc.body);
+  delegate.on('click', 'a', function (e, targ) {
+    var loc = win.location,
+      curr = loc.protocol + "//" + loc.host;
+
+    if (loc.href === targ.href) {
+      e.preventDefault();
+      return;
+    }
+    if (targ.href.indexOf(curr) !== 0 ) {
+      return true;
+    }
+    e.preventDefault();
+    getPage(targ.pathname, function (data) {
+      replacePage(data);
+      pushUrl(targ.pathname);
+    });
+  });
+
+  win.addEventListener('load', function() {
+    new FastClick(doc.body);
+  }, false);
+
+})(this);
